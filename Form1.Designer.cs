41598d0536bab45b06a484bc39312f1f276b1c12
Oswald Comber <oswald.comber@hotmail.com>
2025-08-29 00:29:36 +0200

    Initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             panelMenu = new Panel();
+            checkBoxAlle = new CheckBox();
             label1 = new Label();
             buttonSave = new Button();
             buttonOpen = new Button();
-            checkBoxDiakritika = new CheckBox();
-            checkBoxUmlaute = new CheckBox();
+            checkBoxUmlauteDiakrit = new CheckBox();
             checkBoxMehrereNamen = new CheckBox();
             checkBoxDoppelnamen = new CheckBox();
             menuStrip1 = new MenuStrip();
@@ -48,7 +48,6 @@
             textBoxCleaned = new TextBox();
             saveFileDialog1 = new SaveFileDialog();
             openFileDialog1 = new OpenFileDialog();
-            checkBoxAlle = new CheckBox();
             panelMenu.SuspendLayout();
             menuStrip1.SuspendLayout();
             SuspendLayout();
@@ -59,8 +58,7 @@
             panelMenu.Controls.Add(label1);
             panelMenu.Controls.Add(buttonSave);
             panelMenu.Controls.Add(buttonOpen);
-            panelMenu.Controls.Add(checkBoxDiakritika);
-            panelMenu.Controls.Add(checkBoxUmlaute);
+            panelMenu.Controls.Add(checkBoxUmlauteDiakrit);
             panelMenu.Controls.Add(checkBoxMehrereNamen);
             panelMenu.Controls.Add(checkBoxDoppelnamen);
             panelMenu.Controls.Add(menuStrip1);
@@ -70,11 +68,22 @@
             panelMenu.Size = new Size(1750, 111);
             panelMenu.TabIndex = 0;
             // 
+            // checkBoxAlle
+            // 
+            checkBoxAlle.AutoSize = true;
+            checkBoxAlle.Location = new Point(648, 65);
+            checkBoxAlle.Name = "checkBoxAlle";
+            checkBoxAlle.Size = new Size(57, 24);
+            checkBoxAlle.TabIndex = 9;
+            checkBoxAlle.Text = "Alle";
+            checkBoxAlle.UseVisualStyleBackColor = true;
+            checkBoxAlle.CheckedChanged += checkBoxAlle_CheckedChanged;
+            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
-            label1.Location = new Point(227, 38);
+            label1.Location = new Point(165, 35);
             label1.Name = "label1";
             label1.Size = new Size(307, 20);
             label1.TabIndex = 8;
@@ -89,6 +98,7 @@
             buttonSave.Size = new Size(62, 62);
             buttonSave.TabIndex = 7;
             buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += SpeichernToolStripMenuItem_Click;
             // 
             // buttonOpen
             // 
@@ -99,46 +109,40 @@
             buttonOpen.Size = new Size(62, 62);
             buttonOpen.TabIndex = 6;
             buttonOpen.UseVisualStyleBackColor = true;
+            buttonOpen.Click += OeffnenToolStripMenuItem_Click;
             // 
-            // checkBoxDiakritika
+            // checkBoxUmlauteDiakrit
             // 
-            checkBoxDiakritika.AutoSize = true;
-            checkBoxDiakritika.Location = new Point(614, 68);
-            checkBoxDiakritika.Name = "checkBoxDiakritika";
-            checkBoxDiakritika.Size = new Size(164, 24);
-            checkBoxDiakritika.TabIndex = 5;
-            checkBoxDiakritika.Text = "Diakritische Zeichen";
-            checkBoxDiakritika.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxUmlaute
-            // 
-            checkBoxUmlaute.AutoSize = true;
-            checkBoxUmlaute.Location = new Point(521, 68);
-            checkBoxUmlaute.Name = "checkBoxUmlaute";
-            checkBoxUmlaute.Size = new Size(87, 24);
-            checkBoxUmlaute.TabIndex = 4;
-            checkBoxUmlaute.Text = "Umlaute";
-            checkBoxUmlaute.UseVisualStyleBackColor = true;
+            checkBoxUmlauteDiakrit.AutoSize = true;
+            checkBoxUmlauteDiakrit.Location = new Point(459, 65);
+            checkBoxUmlauteDiakrit.Name = "checkBoxUmlauteDiakrit";
+            checkBoxUmlauteDiakrit.Size = new Size(183, 24);
+            checkBoxUmlauteDiakrit.TabIndex = 4;
+            checkBoxUmlauteDiakrit.Text = "Umlaute und Diakritika";
+            checkBoxUmlauteDiakrit.UseVisualStyleBackColor = true;
+            checkBoxUmlauteDiakrit.CheckedChanged += checkBoxUmlauteDiakrit_CheckedChanged;
             // 
             // checkBoxMehrereNamen
             // 
             checkBoxMehrereNamen.AutoSize = true;
-            checkBoxMehrereNamen.Location = new Point(358, 68);
+            checkBoxMehrereNamen.Location = new Point(296, 65);
             checkBoxMehrereNamen.Name = "checkBoxMehrereNamen";
             checkBoxMehrereNamen.Size = new Size(157, 24);
             checkBoxMehrereNamen.TabIndex = 3;
             checkBoxMehrereNamen.Text = "Mehrere Vornamen";
             checkBoxMehrereNamen.UseVisualStyleBackColor = true;
+            checkBoxMehrereNamen.CheckedChanged += checkBoxMehrereNamen_CheckedChanged;
             // 
             // checkBoxDoppelnamen
             // 
             checkBoxDoppelnamen.AutoSize = true;
-            checkBoxDoppelnamen.Location = new Point(226, 68);
+            checkBoxDoppelnamen.Location = new Point(164, 65);
             checkBoxDoppelnamen.Name = "checkBoxDoppelnamen";
             checkBoxDoppelnamen.Size = new Size(126, 24);
             checkBoxDoppelnamen.TabIndex = 2;
             checkBoxDoppelnamen.Text = "Doppelnamen";
             checkBoxDoppelnamen.UseVisualStyleBackColor = true;
+            checkBoxDoppelnamen.CheckedChanged += checkBoxDoppelnamen_CheckedChanged;
             // 
             // menuStrip1
             // 
@@ -160,36 +164,38 @@
             // oeffnenToolStripMenuItem
             // 
             oeffnenToolStripMenuItem.Name = "oeffnenToolStripMenuItem";
-            oeffnenToolStripMenuItem.Size = new Size(195, 26);
+            oeffnenToolStripMenuItem.Size = new Size(224, 26);
             oeffnenToolStripMenuItem.Text = "Ö&ffnen";
-            oeffnenToolStripMenuItem.Click += oeffnenToolStripMenuItem_Click;
+            oeffnenToolStripMenuItem.Click += OeffnenToolStripMenuItem_Click;
             // 
             // speichernToolStripMenuItem
             // 
             speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
-            speichernToolStripMenuItem.Size = new Size(195, 26);
+            speichernToolStripMenuItem.Size = new Size(224, 26);
             speichernToolStripMenuItem.Text = "&Speichern";
+            speichernToolStripMenuItem.Click += SpeichernToolStripMenuItem_Click;
             // 
             // speicherunterToolStripMenuItem
             // 
             speicherunterToolStripMenuItem.Name = "speicherunterToolStripMenuItem";
-            speicherunterToolStripMenuItem.Size = new Size(195, 26);
+            speicherunterToolStripMenuItem.Size = new Size(224, 26);
             speicherunterToolStripMenuItem.Text = "Speichern &unter";
-            speicherunterToolStripMenuItem.Click += speicherunterToolStripMenuItem_Click;
+            speicherunterToolStripMenuItem.Click += SpeicherunterToolStripMenuItem_Click;
             // 
             // toolStripMenuItem1
             // 
             toolStripMenuItem1.Name = "toolStripMenuItem1";
-            toolStripMenuItem1.Size = new Size(192, 6);
+            toolStripMenuItem1.Size = new Size(221, 6);
             // 
             // exitToolStripMenuItem
             // 
             exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            exitToolStripMenuItem.Size = new Size(195, 26);
+            exitToolStripMenuItem.Size = new Size(224, 26);
             exitToolStripMenuItem.Text = "&Exit";
             // 
             // textBoxSource
             // 
+            textBoxSource.BackColor = Color.Ivory;
             textBoxSource.Dock = DockStyle.Left;
             textBoxSource.Location = new Point(0, 111);
             textBoxSource.Multiline = true;
@@ -201,6 +207,7 @@
             // 
             // textBoxCleaned
             // 
+            textBoxCleaned.BackColor = Color.Honeydew;
             textBoxCleaned.Dock = DockStyle.Fill;
             textBoxCleaned.Location = new Point(884, 111);
             textBoxCleaned.Multiline = true;
@@ -213,16 +220,6 @@
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // checkBoxAlle
-            // 
-            checkBoxAlle.AutoSize = true;
-            checkBoxAlle.Location = new Point(784, 68);
-            checkBoxAlle.Name = "checkBoxAlle";
-            checkBoxAlle.Size = new Size(57, 24);
-            checkBoxAlle.TabIndex = 9;
-            checkBoxAlle.Text = "Alle";
-            checkBoxAlle.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -255,8 +252,7 @@
         private ToolStripMenuItem exitToolStripMenuItem;
         private TextBox textBoxSource;
         private TextBox textBoxCleaned;
-        private CheckBox checkBoxDiakritika;
-        private CheckBox checkBoxUmlaute;
+        private CheckBox checkBoxUmlauteDiakrit;
         private CheckBox checkBoxMehrereNamen;
         private CheckBox checkBoxDoppelnamen;
         private Button buttonOpen;
--- Form1.cs
+++ Form1.cs
@@ -1,40 +1,263 @@
+﻿using System.Text;
+
 namespace Sokrates_Cleanup_for_AD
 {
     public partial class Form1 : Form
     {
+        string LetzterExportPfad = null; // global speichern
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void oeffnenToolStripMenuItem_Click(object sender, EventArgs e)
+        private string ExtractSubstringsBySeparators(string input)
+        {
+            var separators = new List<char>();
+
+            if (checkBoxDoppelnamen.Checked)
+                separators.Add('-');
+
+            if (checkBoxMehrereNamen.Checked)
+                separators.Add(' ');
+
+            var lines = input.Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            var resultLines = new List<string>();
+
+            foreach (var line in lines)
+            {
+                var fields = line.Split(';');
+                var extractedFields = new List<string>();
+
+                foreach (var field in fields)
+                {
+                    if (string.IsNullOrWhiteSpace(field))
+                    {
+                        extractedFields.Add("");
+                        continue;
+                    }
+
+                    if (separators.Count == 0)
+                    {
+                        extractedFields.Add(field);
+                        continue;
+                    }
+
+                    int firstIndex = field.Length;
+
+                    foreach (var sep in separators)
+                    {
+                        int index = field.IndexOf(sep);
+                        if (index != -1 && index < firstIndex)
+                            firstIndex = index;
+                    }
+
+                    string extracted = firstIndex == field.Length ? field : field.Substring(0, firstIndex);
+                    extractedFields.Add(extracted);
+                }
+
+                resultLines.Add(string.Join(";", extractedFields));
+            }
+
+            return string.Join(Environment.NewLine, resultLines);
+        }
+
+        private string CleanUmlauteDiakritika(string input)
+        {
+            var replacements = new Dictionary<string, string>
+            {
+                // Deutsche Umlaute
+                { "Ä", "AE" }, { "ä", "ae" },
+                { "Ö", "OE" }, { "ö", "oe" },
+                { "Ü", "UE" }, { "ü", "ue" },
+                { "ß", "ss" },
+
+                // Slawische & weitere Zeichen
+                { "Č", "C" }, { "č", "c" },
+                { "Š", "S" }, { "š", "s" },
+                { "Ž", "Z" }, { "ž", "z" },
+                { "Ć", "C" }, { "ć", "c" },
+                { "Đ", "D" }, { "đ", "d" },
+                { "Ł", "L" }, { "ł", "l" },
+                { "Ń", "N" }, { "ń", "n" },
+                { "Ř", "R" }, { "ř", "r" },
+                { "Ą", "A" }, { "ą", "a" },
+                { "Ę", "E" }, { "ę", "e" },
+                { "Ň", "N" }, { "ň", "n" },
+                { "Ľ", "L" }, { "ľ", "l" },
+                { "Ť", "T" }, { "ť", "t" },
+                { "Ŕ", "R" }, { "ŕ", "r" },
+                { "Ď", "D" }, { "ď", "d" },
+                { "Ő", "O" }, { "ő", "o" },
+                { "Ű", "U" }, { "ű", "u" },
+
+                // Türkisch
+                { "Ç", "C" }, { "ç", "c" },
+                { "Ş", "S" }, { "ş", "s" },
+                { "Ğ", "G" }, { "ğ", "g" },
+                { "İ", "I" }, { "ı", "i" },
+
+                // Rumänisch
+                { "Ă", "A" }, { "ă", "a" },
+                { "Â", "A" }, { "â", "a" },
+                { "Î", "I" }, { "î", "i" },
+                { "Ș", "S" }, { "ș", "s" },
+                { "Ț", "T" }, { "ț", "t" },
+
+                // Ergänzt: Í, í, Ó, ó
+                { "Í", "I" }, { "í", "i" },
+                { "Ó", "O" }, { "ó", "o" }
+            };
+
+            bool erstesVorkommenlassen = true;
+            var builder = new StringBuilder(input.Length);
+
+            foreach (char c in input)
+            {
+                if (c == 'ü')
+                {
+                    if (erstesVorkommenlassen)
+                    {
+                        builder.Append('ü'); // erstes ü bleibt erhalten
+                        erstesVorkommenlassen = false;
+                    }
+                }
+                else
+                {
+                    string key = c.ToString();
+                    builder.Append(replacements.ContainsKey(key) ? replacements[key] : key);
+                }
+
+            }
+
+            return builder.ToString();
+        }
+
+        private int ParseOutput()
+        {
+            string workingText = textBoxSource.Text;
+            bool nichtszutun = true;
+
+            if (checkBoxUmlauteDiakrit.Checked)
+            {
+                workingText = CleanUmlauteDiakritika(workingText);
+                nichtszutun = false;
+            }
+
+
+            if (checkBoxDoppelnamen.Checked || checkBoxMehrereNamen.Checked)
+            {
+                workingText = ExtractSubstringsBySeparators(workingText);
+                nichtszutun = false;
+            }
+
+
+            // Nur wenn keine Checkbox aktiv war → Originaltext übernehmen
+            if (nichtszutun)
+                workingText = textBoxSource.Text;
+
+            textBoxCleaned.Text = workingText;
+            return 0;
+        }
+
+        private void OeffnenToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Konfiguration des OpenFileDialogs
-            openFileDialog1.Title = "CSV-Datei �ffnen";
+            openFileDialog1.Title = "CSV-Datei öffnen";
             openFileDialog1.Filter = "CSV-Dateien (*.csv)|*.csv|Textdateien (*.txt)|*.txt|Alle Dateien (*.*)|*.*";
             openFileDialog1.Multiselect = false;
 
-            // Dialog anzeigen und pr�fen, ob eine Datei ausgew�hlt wurde
+            // Dialog anzeigen und prüfen, ob eine Datei ausgewählt wurde
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
                 {
                     // Dateiinhalt lesen
-                    string dateiInhalt = File.ReadAllText(openFileDialog1.FileName);
+                    string filecontent = File.ReadAllText(openFileDialog1.FileName);
 
                     // Inhalt in die TextBox schreiben
-                    textBoxSource.Text = dateiInhalt;
+                    textBoxSource.Text = filecontent;
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show("Fehler beim �ffnen der Datei:\n" + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Fehler beim Öffnen der Datei:\n" + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            ParseOutput();
+        }
+
+        private void SpeicherunterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Konfiguration des SaveFileDialogs
+            saveFileDialog1.Title = "CSV-Datei speichern unter";
+            saveFileDialog1.Filter = "CSV-Dateien (*.csv)|*.csv";
+            saveFileDialog1.DefaultExt = "csv";
+            saveFileDialog1.AddExtension = true;
+
+            // Dialog anzeigen und prüfen, ob ein Speicherort gewählt wurde
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Inhalt aus der TextBox holen
+                    string content = textBoxCleaned.Text;
+
+                    // Inhalt in die gewählte Datei schreiben
+                    File.WriteAllText(saveFileDialog1.FileName, content, Encoding.UTF8);
+
+                    MessageBox.Show("Datei erfolgreich gespeichert.", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LetzterExportPfad = saveFileDialog1.FileName; // Pfad speichern
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Fehler beim Speichern:\n" + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
 
-        private void speicherunterToolStripMenuItem_Click(object sender, EventArgs e)
+        private void SpeichernToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            string content = textBoxCleaned.Text;
 
+            if (string.IsNullOrWhiteSpace(LetzterExportPfad))
+            {
+                // Kein Pfad vorhanden → "Speichern unter" auslösen
+                SpeicherunterToolStripMenuItem_Click(sender, e);
+                return;
+            }
+
+            try
+            {
+                File.WriteAllText(LetzterExportPfad, content, Encoding.UTF8);
+                MessageBox.Show($"Datei erfolgreich gespeichert unter:\n{Path.GetFullPath(LetzterExportPfad)}", "Erfolg", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fehler beim Schnellspeichern:\n" + ex.Message, "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void checkBoxAlle_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxDoppelnamen.Checked = true;
+            checkBoxMehrereNamen.Checked = true;
+            checkBoxUmlauteDiakrit.Checked = true;
+        }
+
+        private void checkBoxMehrereNamen_CheckedChanged(object sender, EventArgs e)
+        {
+            ParseOutput();
+        }
+
+        private void checkBoxDoppelnamen_CheckedChanged(object sender, EventArgs e)
+        {
+            ParseOutput();
+        }
+
+        private void checkBoxUmlauteDiakrit_CheckedChanged(object sender, EventArgs e)
+        {
+            ParseOutput();
         }
     }
 }
